3d8ef11ab9517865cdb5180dff0d7812c0574230
Bhagwan7989 <116991187+Bhagwan7989@users.noreply.github.com>
2025-09-19 16:45:52 +0530

    Update DBConnection.java
    bhagwan

--- TransactionReportApp/src/main/java/dao/DBConnection.java
+++ TransactionReportApp/src/main/java/dao/DBConnection.java
@@ -6,8 +6,8 @@ import java.sql.DriverManager;
 public class DBConnection {
 
     private static final String URL = "jdbc:mysql://localhost:3306/transactiondb";
-    private static final String USER = "root";      // change if needed
-    private static final String PASS = "bhagwan@7989";      // change if needed
+    private static final String USER = "root";      
+    private static final String PASS = "bhagwan@7989";      
 
     public static Connection getConnection() throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
